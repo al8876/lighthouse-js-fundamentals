13e9ee142a00ddd5abd4124d429f54676775f58d
Alex Lee <al8876@gmail.com>
2017-12-15 06:38:55 +0000

    ingredients exercise

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+
+  var list = 0;
+  while (list < ingredients.length) {
+    console.log(ingredients[list]);
+    list++;
+  }
+
+  for (var i = 0; i < ingredients.length; i++) {
+    console.log(ingredients[i]);
+  }
+
+  for (var i = ingredients.length; i >= 0; i --) {
+    console.log(ingredients[i]);
+  }
